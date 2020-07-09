f20964057d77c83fd2dbc5afb8da347b8c3b7585
von Buelow <jaspar@UMICH.EDU>
2020-07-09 12:41:41 -0400

    Test left arrow

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -18,6 +18,19 @@ it("works when you click on the right arrow", function() {
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
 });
 
+it("works when you click on the left arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  
+  // move backward in the carousel
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+});
+
 // smoke test
 it("renders without crashing", function() {
   render(<Carousel />);
